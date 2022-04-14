4a51700c83ceaec0079b645ff1e0fa8dff75e86e
CacheMoneyPlaya <janvanbergen1234@gmail.com>
2022-04-15 00:46:19 +0100

    predicts data in test well

--- main.py
+++ main.py
@@ -39,8 +39,8 @@ data['log_return'] = np.log1p(data['Close'].pct_change())
 data = data.dropna(subset=['Close'])
 data.reset_index(inplace=True)
 train, test = train_test_split(data, test_size=0.2, shuffle=False)
-sns.lineplot(x=train['Datetime'], y=train['Close'], data=train, palette=['green'])
-sns.lineplot(x=test['Datetime'], y=test['Close'], data=test, palette=['red'])
+# sns.lineplot(x=train['Datetime'], y=train['Close'], data=train, palette=['green'])
+# sns.lineplot(x=test['Datetime'], y=test['Close'], data=test, palette=['red'])
 
 # split a univariate sequence into samples
 def split_sequence(sequence, n_steps):
@@ -71,6 +71,7 @@ scaler = MinMaxScaler()
 scaled_train = scaler.fit_transform(X)
 scaler_y = MinMaxScaler()
 scaled_y = scaler_y.fit_transform(y.reshape(-1,1))
+test_y = y
 
 X,y = scaled_train, scaled_y
 
@@ -109,9 +110,58 @@ else:
 	model = load_model('model_save')
 
 print('RUNNING MODEL PREDICTION')
+# pred = model.predict(X)
+
+
+#y will contain each value of n[0]+5 shifted
+#pred will contain ~ what y should be in each segment
+
+# ------------------------------------------------
+# define input sequence being log returns
+raw_seq = test['Close']
+
+# choose a number of time steps
+n_steps = 5
+
+# split into samples
+X, y = split_sequence(raw_seq, n_steps)
+
+scaler = MinMaxScaler()
+scaled_test = scaler.fit_transform(X)
+scaler_y = MinMaxScaler()
+scaled_y = scaler_y.fit_transform(y.reshape(-1,1))
+
+X,y = scaled_test, scaled_y
+
+# Push the items into
+	# [
+	# 	[
+	# 		[1],[2],[3],[4],[5]
+	# 	],
+	# ]
+
+n_features = 1
+X = X.reshape((X.shape[0], X.shape[1], n_features))
+# ---------------------------------------------
+
+
 pred = model.predict(X)
 
-print('DONE')
+inverse_pred = scaler_y.inverse_transform(pred);
 
-print(pred[:5])
-print(y[:5])
+inverse_y = scaler_y.inverse_transform(y);
+
+
+# sns.lineplot(data = inverse_y)
+# sns.lineplot(data = inverse_pred, palette = ['orange'])
+# plt.legend(labels = ['Test', 'Predicted'], loc = 'best', fontsize = 'large')
+# plt.xticks(np.arange(len(y),step=50),test['Datetime'].index)
+# plt.xlabel('Date', fontsize=5)
+# plt.ylabel('Price', fontsize=5)
+# plt.show()
+date = test['Datetime'][6:]
+
+fig, ax = plt.subplots()
+ax.plot_date(date, inverse_y, '-b')
+ax.plot_date(date, inverse_pred, '-r')
+plt.show()
